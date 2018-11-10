4aba09abf5dea1737c925125687aef1c15fb770c
Jerold Kim <iamjooyoung@gmail.com>
2018-11-10 23:59:00 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -15,3 +15,12 @@ end
 def last_element(array)
   array[-1]
 end 
+def first_element_with_array_methods(a)
+  a.first 
+end
+def last_element_with_array_methods(a)
+  a.last
+end 
+def length_of_array(a)
+  a.length 
+end
